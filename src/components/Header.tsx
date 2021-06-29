df10be67da3d7979c17976ea1c1728264e439687
Mateo Galic <mateogalic112@gmail.com>
2021-06-30 00:26:23 +0200

    Adding better header.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,27 +1,14 @@
 import React from "react";
 
-import { useLocation } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
 
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
-import {
-  AppBar,
-  Toolbar,
-  Button,
-  IconButton,
-  Typography,
-} from "@material-ui/core";
-import MenuIcon from "@material-ui/icons/Menu";
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    root: {
-      flexGrow: 1,
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
     title: {
-      flexGrow: 1,
+      marginRight: "auto",
     },
   })
 );
@@ -46,18 +33,19 @@ const Header: React.FC = () => {
     <header>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" className={classes.title}>
             {displayPageName()}
           </Typography>
-          <Button color="inherit">User | Login</Button>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Feed</Link>
+              </li>
+              <li>
+                <Link to="/athletes">Athletes</Link>
+              </li>
+            </ul>
+          </nav>
         </Toolbar>
       </AppBar>
     </header>
